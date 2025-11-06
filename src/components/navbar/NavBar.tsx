93d711548eec2881beebd9e7ba6c6e834054d8a7
Gabriella <gabriella.parra05@gmail.com>
2025-11-06 13:26:44 -0300

    alteração navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,24 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <>
-            <div className='w-full flex justify-center py-4
-            			   bg-green-800 text-white'>
-                           
-
-            
+            <div
+                className="w-full flex justify-center py-4
+            			   bg-green-800 text-white"
+            >
                 <div className="container flex justify-between text-lg mx-8">
-                     <Link to='/home' className="text-2xl font-boold text-white">Farmacia Plantas e Raízes</Link>
+                    <Link to="/home" className="text-2xl font-boold text-white">
+                        Farmacia Plantas e Raízes
+                    </Link>
 
-                    <div className='flex gap-4'>
-                        Listas de Categorias
-                        Cadastrar Categoria
+                    <div className="flex gap-4">
+                        <Link
+                            to="/categorias"
+                            className="hover:underline hover:cursor-pointer ml-5"
+                        >
+                            Listas Categoria
+                        </Link>
+                        <Link
+                            to="/cadastrarcategoria"
+                            className="hover:underline hover:cursor-pointer ml-5"
+                        >
+                            Cadastrar Categoria
+                        </Link>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
